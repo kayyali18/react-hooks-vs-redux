e543a963d24fb92c5a8fa40e356a996fe655af35
Ahmad Kayyali <kayyali18@gmail.com>
2021-01-01 14:10:20 -0500

    Add context to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import ProductsPage from './pages/Products';
-import CartPage from './pages/Cart';
-import './App.css';
+import ShopContext from "./context/shop-context";
+import ProductsPage from "./pages/Products";
+import CartPage from "./pages/Cart";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    products: [
+      { id: "p1", title: "Gaming Mouse", price: 29.99 },
+      { id: "p2", title: "Harry Potter 3", price: 9.99 },
+      { id: "p3", title: "Used plastic bottle", price: 0.99 },
+      { id: "p4", title: "Half-dried plant", price: 2.99 },
+    ],
+    cart: [],
+  };
+
+  addProductToCart = (product) => {
+    console.log("Adding product", product);
+  };
+
+  removeProductFromCart = (productId) => {
+    console.log("Removing product with id: ", productId);
+  };
+
   render() {
+    const { products, cart } = this.state;
+    const { addProductToCart, removeProductFromCart } = this;
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={ProductsPage} exact />
-          <Route path="/cart" component={CartPage} exact />
-        </Switch>
-      </BrowserRouter>
+      <ShopContext.Provider
+        value={{ products, cart, addProductToCart, removeProductFromCart }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ProductsPage} exact />
+            <Route path="/cart" component={CartPage} exact />
+          </Switch>
+        </BrowserRouter>
+      </ShopContext.Provider>
     );
   }
 }
